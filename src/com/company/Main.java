d4758e9820b45674397923a939a58a5925678d3a
ACER <nuridin02kg02@gmail.com>
2022-05-08 12:42:47 +0600

    CommitTab

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,18 +6,23 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int seed = sc.nextInt();
+
         ClassOop car = new ClassOop();
-        car.setName("Camry");
-        car.setModel("Camry 70");
-        car.setColour("White");
-        car.setYear(2022);
-        car.setSpeed(seed);
+        System.out.println("Write name of car : ");
+        car.setName(sc.nextLine());
+        System.out.println("Write of model car :");
+        car.setModel(sc.nextLine());
+        System.out.println("Write of color in car : ");
+        car.setColour(sc.nextLine());
+        System.out.println("Write year of car : ");
+        car.setYear(sc.nextInt());
+        System.out.println("Write of speed : ");
+        car.setSpeed(sc.nextInt());
         System.out.println("Name : " + car.getName());
         System.out.println("Model : " + car.getModel());
         System.out.println("Color : " + car.getColour());
         System.out.println("Year : " + car.getYear());
         System.out.println("Speed : " + car.getSpeed());
-        car.checkSpeed(seed);
+
     }
 }
